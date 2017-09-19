1f9628e13833d2f3ebdcb352333e54b7a7f2f63d
Ectropy <atomicfusion0@gmail.com>
2017-09-19 09:19:58 -0400

    Commented a line re requiring local auth on certain route.

--- server/router.js
+++ server/router.js
@@ -3,7 +3,7 @@ var passportService = require('./services/passport'); // our service that uses p
 var passport = require('passport'); // passport itself
 var User = require('./models/user'); // include user model
 var requireAuth = passport.authenticate('jwt', {session: false}); // routes with this require authentication to see
-var requireSignin = passport.authenticate('local', {session: false});
+var requireSignin = passport.authenticate('local', {session: false}); // routes with this require local auth
 
 module.exports = function (app) {
 	app.get('/', requireAuth, function (req, res, next) { // a route that requires authentication
