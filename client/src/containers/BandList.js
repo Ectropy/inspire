53c8e5ad497f81e6f0f2f23ca123acc6975ba780
Ectropy <atomicfusion0@gmail.com>
2017-09-20 13:09:10 -0400

    console.logs band names on click

--- client/src/containers/BandList.js
+++ client/src/containers/BandList.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBand } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BandList extends Component {
 	renderList () {
 		return this.props.bands.map((bands) => {
 			return (
-				<li key={bands.name} className="list-group-item">{bands.name}</li>
+				<li key={bands.name} onClick={() => this.props.selectBand(bands)} className="list-group-item">{bands.name}</li>
 			);
 		});
 	}
@@ -28,5 +30,15 @@ function mapStateToProps (state) {
 	};
 }
 
+// All things returned from this function will end up as props on the BandList containter.
+// We need this so we can call the selectBand function above through this.props.selectBand
+// That will start the action >> reducer >> state change process
+function mapDispatchToProps (dispatch) {
+	// Whenever selectBand is called, this will pass the result to ALL of our reducers
+	return bindActionCreators({ selectBand: selectBand }, dispatch);
+}
+
 // export
-export default connect(mapStateToProps)(BandList);
+// Attach matching of the state to props to reducer &
+// attach actions to reducer
+export default connect(mapStateToProps, mapDispatchToProps)(BandList);
